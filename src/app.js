79abc3f13de8f734daa3bd62c91a51037578d745
naxinga <ngarconf@hotmail.com>
2022-06-16 10:31:29 +0000

    finalizado

--- src/app.js
+++ src/app.js
@@ -55,4 +55,29 @@ Carta.addEventListener("click", function() {
   paloAbajo.innerHTML = palitos;
 });
 
-//window.onload = function() {};
+window.onload = function() {
+  function valorAleatorio() {
+    let val = Math.floor(Math.random() * valor.length);
+
+    return valor[val];
+  }
+
+  function paloAleatorio() {
+    let pal = Math.floor(Math.random() * palos.length);
+    if (pal == 0 || pal == 1) {
+      paloArriba.style.color = "red";
+      paloAbajo.style.color = "red";
+    }
+    if (pal == 2 || pal == 3) {
+      paloArriba.style.color = "black";
+      paloAbajo.style.color = "black";
+    }
+    console.log(pal);
+    return palos[pal];
+  }
+
+  let palitos = paloAleatorio();
+  paloArriba.innerHTML = palitos;
+  valorCarta.innerHTML = valorAleatorio();
+  paloAbajo.innerHTML = palitos;
+};
